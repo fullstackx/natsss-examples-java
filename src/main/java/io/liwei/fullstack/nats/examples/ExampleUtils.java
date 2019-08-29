90e41956bc482fdcf9f6803273250d47fa265d4b
veily <drmfly.liw@gmail.com>
2019-08-29 16:00:16 +0800

    消息发布示例支持TLS

---
+++ src/main/java/io/liwei/fullstack/nats/examples/ExampleUtils.java
@@ -0,0 +1,137 @@
+// Copyright 2018 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package io.liwei.fullstack.nats.examples;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.time.Duration;
+
+import io.nats.client.AuthHandler;
+import io.nats.client.Connection;
+import io.nats.client.ConnectionListener;
+import io.nats.client.Consumer;
+import io.nats.client.ErrorListener;
+import io.nats.client.Nats;
+import io.nats.client.Options;
+
+import javax.net.ssl.*;
+
+public class ExampleUtils {
+
+    private static String KEYSTORE_PATH;
+    private static String TRUSTSTORE_PATH;
+    private static String STORE_PASSWORD;
+    private static String KEY_PASSWORD;
+    private static String ALGORITHM = "SunX509";
+
+    public static void initKeyStore(String keyPath, String keyPwd, String trustStorePath, String trustPwd) {
+        KEYSTORE_PATH = keyPath;
+        TRUSTSTORE_PATH = trustStorePath;
+        KEY_PASSWORD = keyPwd;
+        STORE_PASSWORD = trustPwd;
+    }
+
+    public static Options createClusterTLSExampleOptions(
+            String serverUrls, boolean allowReconnect) throws Exception {
+        return createExampleOptions(serverUrls, true, allowReconnect, true);
+    }
+
+    public static Options createExampleOptions(String servers,
+                                               boolean isCluster,
+                                               boolean allowReconnect,
+                                               boolean isTLS) throws Exception {
+        Options.Builder builder = new Options.Builder();
+        if (isCluster) {
+            builder.servers(servers.split(","));
+        } else {
+            builder.server(servers);
+        }
+        if (isTLS) {
+//            SSL
+            builder.sslContext(createContext());
+        }
+        builder.
+                connectionTimeout(Duration.ofSeconds(5)).
+                pingInterval(Duration.ofSeconds(10)).
+                reconnectWait(Duration.ofSeconds(1)).
+                errorListener(new ErrorListener() {
+                    public void exceptionOccurred(Connection conn, Exception exp) {
+                        System.out.println("Exception " + exp.getMessage());
+                    }
+
+                    public void errorOccurred(Connection conn, String type) {
+                        System.out.println("Error " + type);
+                    }
+
+                    public void slowConsumerDetected(Connection conn, Consumer consumer) {
+                        System.out.println("Slow consumer");
+                    }
+                }).
+                connectionListener(new ConnectionListener() {
+                    public void connectionEvent(Connection conn, Events type) {
+                        System.out.println("Status change " + type);
+                    }
+                });
+
+        if (!allowReconnect) {
+            builder = builder.noReconnect();
+        } else {
+            builder = builder.maxReconnects(-1);
+        }
+
+        if (System.getenv("NATS_NKEY") != null && System.getenv("NATS_NKEY") != "") {
+            AuthHandler handler = new ExampleAuthHandler(System.getenv("NATS_NKEY"));
+            builder.authHandler(handler);
+        } else if (System.getenv("NATS_CREDS") != null && System.getenv("NATS_CREDS") != "") {
+            builder.authHandler(Nats.credentials(System.getenv("NATS_CREDS")));
+        }
+
+        return builder.build();
+    }
+
+    private static SSLContext createContext() throws Exception {
+        SSLContext ctx = SSLContext.getInstance(io.nats.client.Options.DEFAULT_SSL_PROTOCOL);
+        ctx.init(createPrivateKeyManagers(), createTrustManagers(), new SecureRandom());
+        return ctx;
+    }
+
+    private static KeyStore loadKeystore(String path) throws Exception {
+        KeyStore store = KeyStore.getInstance("JKS");
+        BufferedInputStream in = null;
+        try {
+            in = new BufferedInputStream(new FileInputStream(path));
+            store.load(in, STORE_PASSWORD.toCharArray());
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        return store;
+    }
+
+    private static KeyManager[] createPrivateKeyManagers() throws Exception {
+        KeyStore store = loadKeystore(KEYSTORE_PATH);
+        KeyManagerFactory factory = KeyManagerFactory.getInstance(ALGORITHM);
+        factory.init(store, KEY_PASSWORD.toCharArray());
+        return factory.getKeyManagers();
+    }
+
+    private static TrustManager[] createTrustManagers() throws Exception {
+        KeyStore store = loadKeystore(TRUSTSTORE_PATH);
+        TrustManagerFactory factory = TrustManagerFactory.getInstance(ALGORITHM);
+        factory.init(store);
+        return factory.getTrustManagers();
+    }
+}
---
+++ src/main/java/io/liwei/fullstack/nats/examples/Publisher.java
@@ -0,0 +1,237 @@
+package io.liwei.fullstack.nats.examples;
+
+import io.nats.client.Connection;
+import io.nats.client.Nats;
+import io.nats.streaming.AckHandler;
+import io.nats.streaming.NatsStreaming;
+import io.nats.streaming.Options;
+import io.nats.streaming.StreamingConnection;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+public class Publisher {
+
+    private String keystorePath;
+    private String truststorePath;
+    private String storePassword;
+    private String keyPassword;
+    private String serverUrls;
+    private String clusterId = "matrixcluster";
+    private String subject = "foo";
+    private String payloadString = "bar";
+
+    private String clientId = "testJavaPub";
+    private boolean async;
+
+
+    private static final String usageString =
+            "\nUsage: java Publisher [options] <subject> <message>\n\nOptions:\n"
+                    + "    -s, --server   <urls>           NATS Streaming server URL(s)\n"
+                    + "    -c, --cluster  <cluster name>   NATS Streaming cluster name\n"
+                    + "    -id,--clientid <client ID>      NATS Streaming client ID\n"
+                    + "    -a, --async                     Asynchronous publish mode";
+
+    public Publisher(String[] args) {
+        parseArgs(args);
+        ExampleUtils.initKeyStore(keystorePath, keyPassword, truststorePath, storePassword);
+
+    }
+
+    private static void usage() {
+        System.err.println(usageString);
+    }
+
+    public void run() throws Exception {
+
+        Options opts = NatsStreaming.defaultOptions();
+
+        Connection nc;
+        if (serverUrls.startsWith("tls://")) {
+            nc = Nats.connect(ExampleUtils.createClusterTLSExampleOptions(serverUrls, true));
+        } else {
+            nc = Nats.connect(ExampleUtils.createExampleOptions(serverUrls,
+                    true, true, false));
+        }
+
+        if (nc != null) {
+            opts = new Options.Builder().natsConn(nc).build();
+        }
+
+        System.out.println(serverUrls);
+        try (StreamingConnection sc = NatsStreaming.connect(clusterId, clientId, opts)) {
+            final CountDownLatch latch = new CountDownLatch(1);
+            final String[] guid = new String[1];
+            byte[] payload = payloadString.getBytes();
+
+            AckHandler acb = new AckHandler() {
+                @Override
+                public void onAck(String nuid, Exception ex) {
+                    System.out.printf("Received ACK for guid %s\n", nuid);
+                    if (ex != null) {
+                        System.err.printf("Error in server ack for guid %s: %s", nuid,
+                                ex.getMessage());
+                    }
+                    if (!guid[0].equals(nuid)) {
+                        System.err.printf(
+                                "Expected a matching guid in ack callback, got %s vs %s\n", nuid,
+                                guid[0]);
+                    }
+                    System.out.flush();
+                    latch.countDown();
+                }
+            };
+
+            if (!async) {
+                try {
+                    //noinspection ConstantConditions
+                    sc.publish(subject, payload);
+                } catch (Exception e) {
+                    System.err.printf("Error during publish: %s\n", e.getMessage());
+                    throw (e);
+                }
+                System.out.printf("Published [%s] : '%s'\n", subject, payloadString);
+            } else {
+                try {
+                    //noinspection ConstantConditions
+                    guid[0] = sc.publish(subject, payload, acb);
+                    latch.await();
+                } catch (IOException e) {
+                    System.err.printf("Error during async publish: %s\n", e.getMessage());
+                    throw (e);
+                }
+
+                if (guid[0].isEmpty()) {
+                    String msg = "Expected non-empty guid to be returned.";
+                    System.err.println(msg);
+                    throw new IOException(msg);
+                }
+                System.out.printf("Published [%s] : '%s' [guid: %s]\n", subject, payloadString,
+                        guid[0]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void parseArgs(String[] args) {
+        if (args == null || args.length < 2) {
+            throw new IllegalArgumentException("must supply at least subject and msg");
+        }
+
+        List<String> argList = new ArrayList<String>(Arrays.asList(args));
+
+        // The last two args should be subject and payloadString
+        // get the payloadString and remove it from args
+        payloadString = argList.remove(argList.size() - 1);
+
+        // get the subject and remove it from args
+        subject = argList.remove(argList.size() - 1);
+
+        // Anything left is flags + args
+        Iterator<String> it = argList.iterator();
+        while (it.hasNext()) {
+            String arg = it.next();
+            switch (arg) {
+                case "-s":
+                case "--server":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    serverUrls = it.next();
+                    it.remove();
+                    continue;
+                case "-ks":
+                case "--keystore":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    keystorePath = it.next();
+                    it.remove();
+                    continue;
+                case "-ts":
+                case "--truststore":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    truststorePath = it.next();
+                    it.remove();
+                    continue;
+                case "-ks_pwd":
+                case "--keystore_password":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    keyPassword = it.next();
+                    it.remove();
+                    continue;
+                case "-ts_pwd":
+                case "--truststore_password":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    storePassword = it.next();
+                    it.remove();
+                    continue;
+                case "-c":
+                case "--cluster":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    clusterId = it.next();
+                    it.remove();
+                    continue;
+                case "-id":
+                case "--clientid":
+                    if (!it.hasNext()) {
+                        throw new IllegalArgumentException(arg + " requires an argument");
+                    }
+                    it.remove();
+                    clientId = it.next();
+                    it.remove();
+                    continue;
+                case "-a":
+                case "--async":
+                    async = true;
+                    it.remove();
+                    continue;
+                default:
+                    throw new IllegalArgumentException(
+                            String.format("Unexpected token: '%s'", arg));
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        if (args == null || args.length < 2) {
+            throw new IllegalArgumentException("must supply at least subject and msg");
+        }
+
+        Publisher publisher = new Publisher(args);
+        try {
+            publisher.run();
+        } catch (IllegalArgumentException e) {
+            System.out.flush();
+            System.err.println(e.getMessage());
+            Publisher.usage();
+            System.err.flush();
+            throw e;
+        }
+
+    }
+
+
+}
